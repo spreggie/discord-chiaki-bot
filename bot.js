e729afa6b8661f8b5ebdcc8625eb7fa77628cbad
spreggie <22599845+spreggie@users.noreply.github.com>
2018-01-12 23:23:05 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setStatus('dnd');
+  client.user.setStatus('idle');
   client.user.setGame('#AcidCat7952');
   // client.user.setGame(`something ( ͡° ͜ʖ ͡°) || Jsem na ${client.guilds.size} serverech`, 'https://www.twitch.tv/logout', 1)
 });
