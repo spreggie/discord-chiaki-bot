b9b1c6aa69e7c3295ca22a1e614bfeeec1150d71
cat_on_acid <22599845+spreggie@users.noreply.github.com>
2018-01-28 00:59:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,8 +11,8 @@ client.on('ready', () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setStatus('dnd');
-  client.user.setGame('Mám rád vlaky🚂🚂')
+  client.user.setStatus('dnd')
+  client.user.setGame('Mám rád vlaky🚂🚂');
   // client.user.setGame('@AcidCat#0069', 'https://www.twitch.tv/twitch', 1)
   // client.user.setGame(`something ( ͡° ͜ʖ ͡°) || Jsem na ${client.guilds.size} serverech`, 'https://www.twitch.tv/logout', 1)
  });
