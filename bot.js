4e5efa2ee2d2b1a263c5fe0619cdd97975f46ba2
cat_on_acid <22599845+spreggie@users.noreply.github.com>
2018-01-29 04:10:52 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ client.on('ready', () => {
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
-   channel.send(`<@!323182446835269633> :fuckyou: `);
+   channel.send(`<@!323182446835269633> :---)))`);
  });
 
 client.on("message", async message => {
