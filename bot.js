d2be7d810303825fcc6cf555d6738237d7147c73
AcidCat <22599845+spreggie@users.noreply.github.com>
2018-07-11 00:19:50 +0200

    New //help

--- bot.js
+++ bot.js
@@ -83,32 +83,38 @@ client.on('ready', () => {
   }
 
   if(command === "help") {
-message.channel.send({embed: {
-    color: 3447003,
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
-    },
-    fields: [{
-        name: "🎬 Základní příkazy 🎬",
-        value: "**//avatar** - přímý odkaz na tvůj avatar"
-        value: "**//ping** - spočítá ping bota mezi odeslanou zprávou a její následnou úpravou"
-        value: "**//help** - zobrazí tuhle nápovědu"
-        value: "**//say** - zopakuje odeslanou zprávu"
-      },
-      {
-        name: "💻 Administrátorské příkazy 💻",
-        value: "**//kick** - vyhodí uživatele ze serveru"
-        value: "**//ban** - permanentně zabanuje uživatele"
-      }
-    ],
-    timestamp: new Date(),
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: "© AcidCat"
+    const embed = new Discord.RichEmbed()
+      .setTitle("This is your title, it can hold 256 characters")
+      .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
+      /*
+       * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+       */
+      .setColor(0x00AE86)
+      .setDescription("This is the main body of text, it can hold 2048 characters.")
+      .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
+      .setImage("http://i.imgur.com/yVpymuV.png")
+      .setThumbnail("http://i.imgur.com/p2qNFag.png")
+      /*
+       * Takes a Date object, defaults to current date.
+       */
+      .setTimestamp()
+      .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
+      .addField("This is a field title, it can hold 256 characters",
+        "This is a field value, it can hold 2048 characters.")
+      /*
+       * Inline fields may not display as inline if the thumbnail and/or image is too big.
+       */
+      .addField("Inline Field", "They can also be inline.", true)
+      /*
+       * Blank field, useful to create some space.
+       */
+      .addBlankField(true)
+      .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
+
+      message.channel.send({embed});
+
     }
-  }
-});
+  });
 
 client.on("message", (message) => {
   if(message.content === "cs") {
