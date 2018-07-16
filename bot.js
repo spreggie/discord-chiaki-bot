48a9cc541da4ec8549b19638788da98c94f47d90
AcidCat <22599845+spreggie@users.noreply.github.com>
2018-07-16 18:03:37 +0200

    x

--- bot.js
+++ bot.js
@@ -89,7 +89,7 @@ client.on('ready', () => {
        * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
        */
       .setColor("#FF0000")
-      .setFooter("© AcidCat", "https://cdn.discordapp.com/avatars/323182446835269633/16aa258ba1314e582f4132c778841360.png")
+      .setFooter("© AcidCat", "https://cdn.discordapp.com/avatars/323182446835269633/43f63233be758666447dc90b825dff82.png")
       /*
        * Takes a Date object, defaults to current date.
        */
